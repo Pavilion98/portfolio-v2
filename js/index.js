2fc64850f191fad3165131aa4cacb84b36e654c2
wassim <wassim.alharakeh@mubs.edu.lb>
2021-03-31 16:35:38 +0300

    ready for deployment

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// Jquery smooth scroll
+$('.main-menu li a').on('click', function(e){
+    if(this.hash !== ''){
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top
+        },
+        800
+        );
+    }
+});
